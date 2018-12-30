8dbdc881c44ac0f304ea8380d438d7ab059161e0
Max Wittig <max.wittig95@gmail.com>
2018-12-30 18:19:26 +0100

    feat: allow org_id from env variable

--- mirror.py
+++ mirror.py
@@ -1,41 +1,49 @@
 import requests
 import os
 import github
+import json
+from loguru import logger
 
 
 gitea_token = os.getenv("GITEA_TOKEN")
 gitea_url = os.getenv("GITEA_URL")
+mirror_org = os.getenv("MIRROR_ORG")
 github_token = os.getenv("GITHUB_TOKEN")
-api_url = f"{gitea_url}/api/v1"
+
+gitea_api_url = f"{gitea_url}/api/v1"
 
 
-def migrate(repo, headers):
+def migrate(repo, headers, org_id):
     body = {
         "clone_addr": repo.clone_url,
         "description": repo.description,
         "mirror": True,
         "private": False,
         "repo_name": repo.name,
-        "uid": 3,
+        "uid": org_id,
     }
-    r = requests.post(f"{api_url}/repos/migrate", json=body, headers=headers)
-    print(r.text)
-
-
-def fetch_repos(token):
-    gh = github.Github(token)
-    return gh.get_user().get_repos(affiliation="owner")
-
+    r = requests.post(f"{gitea_api_url}/repos/migrate", json=body, headers=headers)
+    if r.status_code == 201:
+        logger.info(f"Created {repo.name} mirror from GitHub")
+    else:
+        logger.error(f"Could not create {repo.name}. Skipping.")
 
 def main():
-    repos = fetch_repos(github_token)
     headers = {
         "accept": "application/json",
         "Authorization": f"token {gitea_token}",
         "Content-Type": "application/json",
     }
+
+    gh = github.Github(github_token)
+    repos = gh.get_user().get_repos(affiliation="owner")
+    org_id = json.loads(requests.get(
+        f"{gitea_api_url}/orgs/{mirror_org}",
+        headers=headers
+    ).text)["id"]
+
     for repo in repos:
-        migrate(repo, headers)
+        migrate(repo, headers, org_id)
 
 
 if __name__ == "__main__":
